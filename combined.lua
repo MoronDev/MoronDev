ffded1a224a2dd33e502bf87d665ff3893d38d8a
MoronDev <66959723+MoronDev@users.noreply.github.com>
2024-02-25 00:45:39 +0000

    Create combined.lua

---
+++ combined.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/MoronDev/MoronDev/main/antiafk.lua'))()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/MoronDev/MoronDev/main/autopresent.lua'))()
