231ba046a79eae9056e02a945c2ae40b86c0d86d
MoronDev <66959723+MoronDev@users.noreply.github.com>
2024-02-29 21:09:48 +0000

    Create overnightchest.lua

---
+++ overnightchest.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MoronDev/MoronDev/main/hopifbroken.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MoronDev/MoronDev/main/autochest.lua"))()
