edcd985f4948c02baccfbaa330dfafeda966ccb1
MoronDev <66959723+MoronDev@users.noreply.github.com>
2024-02-29 17:33:01 +0000

    Update buyallat.lua

--- buyallat.lua
+++ buyallat.lua
@@ -39,3 +39,88 @@ local args = {
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Merchant_RequestPurchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "RegularMerchant",
+    [2] = 1
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Merchant_RequestPurchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "RegularMerchant",
+    [2] = 2
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Merchant_RequestPurchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "RegularMerchant",
+    [2] = 3
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Merchant_RequestPurchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "RegularMerchant",
+    [2] = 4
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Merchant_RequestPurchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "RegularMerchant",
+    [2] = 5
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Merchant_RequestPurchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "RegularMerchant",
+    [2] = 6
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Merchant_RequestPurchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "RegularMerchant",
+    [2] = 1
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Merchant_RequestPurchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "RegularMerchant",
+    [2] = 2
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Merchant_RequestPurchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "RegularMerchant",
+    [2] = 3
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Merchant_RequestPurchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "RegularMerchant",
+    [2] = 4
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Merchant_RequestPurchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "RegularMerchant",
+    [2] = 5
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Merchant_RequestPurchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "RegularMerchant",
+    [2] = 6
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Merchant_RequestPurchase"):InvokeServer(unpack(args))
+
